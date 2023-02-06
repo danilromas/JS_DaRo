013564edb84e1f39aa3a7a6d34aec823d67e86ee
danilromas <furiblack904@gmail.com>
2023-02-06 18:47:11 +0300

    string

--- script1.js
+++ script1.js
@@ -1,7 +1,7 @@
 "use strict";
-let str;
-str = '!!!';
-alert(str.length); //Коментарий строчный
+let str = `abc
+def`; //Коментарий строчный
+alert(str);
 
 let a, b, c,d; // объявляем все 3 переменные
 
@@ -13,4 +13,12 @@ d = ' world';
 alert(a);
 alert(c+d);
 
+let str1 = 'xxx';
+let str2 = 'yyy';
+let txt  = `aaa${str1}bbb${str2}ccc`;
+alert(txt);
 
+let abc = `f
+g
+k
+`;

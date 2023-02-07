76cc5514ac65e90ed7890ef3a3e9d1b84b3f6f88
danilromas <furiblack904@gmail.com>
2023-02-07 21:39:04 +0300

    last string

--- script1.js
+++ script1.js
@@ -1,7 +1,5 @@
 "use strict";
-let str = `abc
-def`; //Коментарий строчный
-alert(str);
+
 
 let a, b, c,d; // объявляем все 3 переменные
 
@@ -9,16 +7,7 @@ let num1 = 123;
 let num2 = 456;
 console.log(num1, num2);
 
-let str1 = 'абвг';
-
-alert(str1[0]); // выведет 'а'
-alert(str1[1]); // выведет 'б'
-alert(str1[2]); // выведет 'в'
-
-let str2 = 'abcde';
-
-alert(str2[0] + str[2] + str[5]);
-
-let str3 = 'abcde';
-
-alert(str3[4] + str3[3] + str3[2] + str3[1] + str3[0]);
+let str1 = 'abcde';
+let last = str1.length - 1; // номер последнего элемента строки
+alert(str1[last]); // выведет последнюю букву
+alert(str1[last - 1]); // выведет предпоследнюю букву

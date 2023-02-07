8fbe987767302793361d3faee54343ee114156e5
danilromas <furiblack904@gmail.com>
2023-02-07 21:22:19 +0300

    Srting.length

--- script1.js
+++ script1.js
@@ -9,18 +9,7 @@ let num1 = 123;
 let num2 = 456;
 console.log(num1, num2);
 
-let num = parseInt('12px');
-alert(num); // выведет 12
+let num = 12345;
+alert(String(num).length); // длина будет равна 5
 
-let num3 = parseFloat('12.5px');
-alert(num3); // выведет 12.5
-
-a = parseInt('5px');
-b = parseInt('6px');
-
-alert(a + b);
-
-c = parseFloat('5.5px');
-d = parseFloat('6.25px');
-let px = 'px';
-alert(c + d + px);
+alert(String(num1).length + String(num2).length);

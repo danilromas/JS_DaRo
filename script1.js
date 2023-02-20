366b0855db6e3e5747109ffb754e0b09812fa7f9
danilromas <furiblack904@gmail.com>
2023-02-20 20:30:59 +0300

    js_30

--- script1.js
+++ script1.js
@@ -1,11 +1,13 @@
 "use strict";
 
 
-let a = '12345';
-let b = String(12345);
-let c = String(12345);
-let d = String(12345);
-alert(Number(a[0])+Number(a[1])+Number(a[2])+Number(a[3])+Number(a[4]));
-alert(Number(b[0])+Number(b[1])+Number(b[2])+Number(b[3])+Number(b[4]));
-alert(Number(c[0])*Number(c[1])*Number(c[2])*Number(c[3])*Number(c[4]));
-alert((d[4])+(d[3])+(d[2])+(d[1])+(d[0]));
+let a = 47;
+alert(a);
+a += 7;
+alert(a);
+a -= 18;
+alert(a);
+a *= 10;
+alert(a);
+a /= 15;
+alert(a);

915982ac920a8f7036c081d0de4662532b30e276
danilromas <furiblack904@gmail.com>
2023-02-20 21:30:39 +0300

    js_65

--- script1.js
+++ script1.js
@@ -1,4 +1,13 @@
 "use strict";
 
-let obj = {x: 1, y: 2, z: 3};
-console.log( typeof obj['x'] );
+/*let nums1 = [1, 2, 3];
+console.log( typeof nums1 ); - object
+
+
+let nums2 = [1, 2, 3];
+console.log( typeof nums2[0] ); - number
+
+
+let nums3 = ['1', '2', '3'];
+console.log( typeof nums3[0] ); - string 
+*/

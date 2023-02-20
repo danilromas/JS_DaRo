fbd203d50d3864761f7bcedaffa85f8eaa10a126
danilromas <furiblack904@gmail.com>
2023-02-20 21:07:54 +0300

    js_49

--- script1.js
+++ script1.js
@@ -1,4 +1,16 @@
 "use strict";
 
-let obj = {1: 'Monday', 2: 'Tuesday', 3: '', 4: 'Четверг', 5: 'Friday', 6: 'Суббота', 7: 'Воскресенье'};
-alert(obj[1] + obj[2]+ obj[3]+ obj[4]+ obj[5]+ obj[6]+ obj[7])
+let obj = {
+    1: 'January',
+    2: 'february', 
+    3: 'March', 
+    4: 'April', 
+    5: 'May', 
+    6: 'June', 
+    7: 'Juline',
+    8: 'Agust',
+    9: 'Sentepmber',
+    10: 'October',
+    11: 'November',
+    12: 'Decamber'};
+console.log(obj);

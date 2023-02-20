9fa9055a5b1cf4fd7c00bbcb0f722386ad911c27
danilromas <furiblack904@gmail.com>
2023-02-20 21:33:20 +0300

    js_67

--- script1.js
+++ script1.js
@@ -1,18 +1,22 @@
 "use strict";
 
-/*let nums1 = [1, 2, 3];
-console.log( typeof nums1 ); - object
-
-
-let nums2 = [1, 2, 3];
-console.log( typeof nums2[0] ); - number
-
-
-let nums3 = ['1', '2', '3'];
-console.log( typeof nums3[0] ); - string 
-
-console.log( Array.isArray([1, 2, 3]) ); true
-
-console.log( Array.isArray({x: 1, y: 2, z: 3}) ); false
- 
-*/
+let test1 = {x: 1, y: 2, z: 3};
+console.log(test1);
+//object
+let test2 = {x: 1, y: 2, z: 3};
+console.log(test2.x);
+//primitive
+let test3 = [1, 2, 3];
+console.log(test3);
+//object
+let test4 = [1, 2, 3];
+console.log(test4[1]);
+//primitive
+let test5 = [1, 2, 3];
+let test6 = 1;
+console.log(test6);
+//object
+let test7 = [1, 2, 3];
+let test8 = 1;
+console.log(test7[test8]);
+//primitive

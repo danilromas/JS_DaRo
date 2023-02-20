83f7270f1ebfbcec82ca3ed0c1297c55f49c3548
danilromas <furiblack904@gmail.com>
2023-02-20 20:27:42 +0300

    js_29

--- script1.js
+++ script1.js
@@ -1,13 +1,11 @@
 "use strict";
 
 
-let a, b, c,d; // объявляем все 3 переменные
-
-let num1 = 123;
-let num2 = 456;
-console.log(num1, num2);
-
-let str1 = 'abcde';
-let last = str1.length - 1; // номер последнего элемента строки
-alert(str1[last]); // выведет последнюю букву
-alert(str1[last - 1]); // выведет предпоследнюю букву
+let a = '12345';
+let b = String(12345);
+let c = String(12345);
+let d = String(12345);
+alert(Number(a[0])+Number(a[1])+Number(a[2])+Number(a[3])+Number(a[4]));
+alert(Number(b[0])+Number(b[1])+Number(b[2])+Number(b[3])+Number(b[4]));
+alert(Number(c[0])*Number(c[1])*Number(c[2])*Number(c[3])*Number(c[4]));
+alert((d[4])+(d[3])+(d[2])+(d[1])+(d[0]));

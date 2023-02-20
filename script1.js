28e2fff46db4a5e54d4bca5f7cf380e1e937c32f
danilromas <furiblack904@gmail.com>
2023-02-20 21:13:04 +0300

    js_52

--- script1.js
+++ script1.js
@@ -1,14 +1,15 @@
 "use strict";
 
-let user = {
-    name: 'Данил',
-    surname: 'Ромашкан', 
-    otch: 'Алексеевич', 
-};
-let date = {
-    year: '2004',
-    month: 'Agust', 
-    day: '20', } 
-console.log(user);
-alert(user['name'] + ' ' + user['surname']+ ' ' + user['otch']);
-console.log(date.year, date.month, date.day);
+let obj = {
+        '1a': 1,
+        'b2': 2,
+        'с-с': 3,
+        'd 4': 4,
+        'e5': 5
+    };
+    
+console.log(obj['1a']); //+
+console.log(obj.b2);
+console.log(obj['c-c']); //+
+console.log(obj['d 4']); //+
+console.log(obj.e5);

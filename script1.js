73997f0f76a3838bbd272b44d581f6e8100f28ec
danilromas <furiblack904@gmail.com>
2023-02-20 20:43:54 +0300

    js_37

--- script1.js
+++ script1.js
@@ -1,7 +1,31 @@
 "use strict";
 
 
-alert(365*60);
-alert(1024*1024)
-alert(1024*1024*1000)
-alert(1024*1024*1000)
+let radius = 10;
+let s = (radius*3,14)**2
+alert(s)
+
+let a = 10;
+let ss = a**a
+alert(ss)
+
+
+let aa = 4;
+let bb = 3;
+let sss = aa*bb;
+alert(sss)
+
+
+let aaa = 4;
+let bbb = 3;
+let p = 2*(aaa+bbb)
+alert(p)
+
+
+let t = 20;
+let f = (t * 9/5) + 32
+alert(f)
+
+let f1 = 451;
+let t1 = (f-32)/1.8;
+alert(t1)

d624d7c9d009bbe29404358442d2bdedf6e8c2d9
danilromas <furiblack904@gmail.com>
2023-02-20 21:31:38 +0300

    js_66

--- script1.js
+++ script1.js
@@ -10,4 +10,9 @@ console.log( typeof nums2[0] ); - number
 
 let nums3 = ['1', '2', '3'];
 console.log( typeof nums3[0] ); - string 
+
+console.log( Array.isArray([1, 2, 3]) ); true
+
+console.log( Array.isArray({x: 1, y: 2, z: 3}) ); false
+ 
 */

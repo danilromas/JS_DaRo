8d27cffe6a5213b46f730865aa3fd57864a1b85a
danilromas <furiblack904@gmail.com>
2023-02-20 20:47:24 +0300

    js_39

--- script1.js
+++ script1.js
@@ -6,3 +6,9 @@ console.log(nums);
 let strs = ['a', 'b', 'c'];
 alert(strs);
 console.log(strs);
+
+let numss = [1,2,3];
+alert(numss[0]);
+alert(numss[1]);
+alert(numss[2]);
+alert(numss[0]+numss[1]+numss[2]);

578cdadc706214e9406a0d87638b0a143d5f5c6c
danilromas <furiblack904@gmail.com>
2023-02-20 21:10:44 +0300

    js_51

--- script1.js
+++ script1.js
@@ -5,5 +5,10 @@ let user = {
     surname: 'Ромашкан', 
     otch: 'Алексеевич', 
 };
+let date = {
+    year: '2004',
+    month: 'Agust', 
+    day: '20', } 
 console.log(user);
 alert(user['name'] + ' ' + user['surname']+ ' ' + user['otch']);
+console.log(date.year, date.month, date.day);

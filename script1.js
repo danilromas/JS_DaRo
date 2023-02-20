6107b9ef17ff14d7290b8d902ea96fb1b6fa6c97
danilromas <furiblack904@gmail.com>
2023-02-20 20:41:34 +0300

    js_36

--- script1.js
+++ script1.js
@@ -1,17 +1,7 @@
 "use strict";
 
 
-let num = 123;
-let str = String(num);
-console.log(str[str.length-1]);
-
-let a = '123';
-let b = '456';
-let s = Number(a) + Number(b);
-
-console.log(s);
-
-let aa = 1;
-let bb = 2;
-let cc = 3;
-console.log(aa + bb + сс);
+alert(365*60);
+alert(1024*1024)
+alert(1024*1024*1000)
+alert(1024*1024*1000)

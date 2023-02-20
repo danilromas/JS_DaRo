11847dbbd31a271d188d3577a1cff5ef8a14b6c0
danilromas <furiblack904@gmail.com>
2023-02-20 21:34:26 +0300

    js_68

--- script1.js
+++ script1.js
@@ -1,22 +1,13 @@
 "use strict";
 
-let test1 = {x: 1, y: 2, z: 3};
-console.log(test1);
-//object
-let test2 = {x: 1, y: 2, z: 3};
-console.log(test2.x);
-//primitive
-let test3 = [1, 2, 3];
-console.log(test3);
-//object
-let test4 = [1, 2, 3];
-console.log(test4[1]);
-//primitive
-let test5 = [1, 2, 3];
-let test6 = 1;
-console.log(test6);
-//object
-let test7 = [1, 2, 3];
-let test8 = 1;
-console.log(test7[test8]);
-//primitive
+let nums1 = [1, 2, 3];
+let nums2 = nums1;
+
+nums1[0] = 'a';
+console.log(nums2);
+
+let nums3 = [1, 2, 3];
+let nums4 = nums3;
+nums3[0] = 'a';
+nums4[1] = 'b';
+console.log(nums3);

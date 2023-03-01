4c7602b4bd082b6e202212a937fd228b7539dc8f
danilromas <furiblack904@gmail.com>
2023-03-01 10:12:48 +0300

    100_ЮБИЛЕЙ

--- script1.js
+++ script1.js
@@ -1,15 +1,8 @@
 "use strict";
-let a = 2 * (3 - 1);
-let b = 6 - 2;
+let ok = confirm('Сколько вам лет?');
 
-console.log(a == b);
-
-let a1 = 5 * (7 - 4);
-let b1 = 1 + 2 + 7;
-
-console.log(a1 > b1);
-
-let a2 = 2 ** 4;
-let b2 = 4 ** 2;
-
-console.log(a2 != b2);
+if (ok>18) {
+	console.log('))))');
+} else {
+	console.log('малой');
+}

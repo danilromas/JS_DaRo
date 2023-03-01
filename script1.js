37f634a2807b6b578442fa142488d66bce42908c
danilromas <furiblack904@gmail.com>
2023-03-01 10:07:51 +0300

    95_if_if

--- script1.js
+++ script1.js
@@ -1,24 +1,12 @@
 "use strict";
-let test = 6;
+let num = 3;
 
-if (num <=9 && num >=1) 
-	if (num == 1) {
-		console.log('1');
-	} else if (num == 2) {
-		console.log('1');
-	} else if (num == 3) {
-		console.log('1');
-	} else if (num == 4) {
-		console.log('1');
-	} else if (num == 5) {
-		console.log('1');
-	} else if (num == 6) {
-		console.log('1');
-	} else if (num == 7) {
-		console.log('1');
-	} else if (num == 8) {
-		console.log('1');
-	} else if (num == 9) {
-		console.log('1');
-    }
-else console.log('неверное значение переменной num');
+if (num >= 0) {
+	if (num <= 5) {
+		console.log('меньше или равно 5');
+	} else {
+		console.log('больше 5');
+	}
+} else {
+	console.log('меньше нуля');
+}

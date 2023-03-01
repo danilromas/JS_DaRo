1472977e90f6a46dc90954a1f9429ccd56efb83a
danilromas <furiblack904@gmail.com>
2023-03-01 09:14:14 +0300

    74_==

--- script1.js
+++ script1.js
@@ -1,30 +1,10 @@
 "use strict";
 
 let test = 10;
-let test1 = 9;
-let test2 = 10;
-let test3 = 10;
 
-if (test > 10) {
-	console.log('+++');
-} else {
-	console.log('---'); // сработает это
-}
 
-if (test1 < 10) {
-	console.log('+++');
+if (test == 10) {
+	console.log('+++'); 
 } else {
-	console.log('---'); // сработает это
-}
-
-if (test2 <= 10) {
-	console.log('+++');
-} else {
-	console.log('---'); // сработает это
-}
-
-if (test3 >= 10) {
-	console.log('+++');
-} else {
-	console.log('---'); // сработает это
+	console.log('---');
 }

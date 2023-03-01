09fe8e93a75ac6beec28dab7cfa0371a1e99ad90
danilromas <furiblack904@gmail.com>
2023-03-01 09:18:39 +0300

    76_сравнение

--- script1.js
+++ script1.js
@@ -3,7 +3,16 @@
 let test = 10;
 
 
-if (test != 10) {
+let test1 = 1;
+let test2 = 2;
+
+if (test2 > test1) {
+	console.log(test2); // сработает это
+} else {
+	console.log(test1);
+}
+
+if (test2 == test1) {
 	console.log('+++'); // сработает это
 } else {
 	console.log('---');

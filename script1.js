d89e232b38af9c1d94a32970ce3a1d6b39fc0f97
danilromas <furiblack904@gmail.com>
2023-03-01 10:00:35 +0300

    88_сокращение

--- script1.js
+++ script1.js
@@ -1,39 +1,9 @@
 "use strict";
-let test = 1;
 
-if (test == false) {
+let test = true;
+
+if (test) {
 	console.log('+++');
 } else {
-	console.log('');
-}
-
-let test = 1;
-
-if (test != true) {
-	console.log('+++');
-} else {
-	console.log('');
-}
-
-let test = '';
-
-if (test == false) {
-	console.log('+++');
-} else {
-	console.log('');
-}
-
-let test;
-
-if (test == true) {
-	console.log('+++');
-} else {
-	console.log('');
-}
-let test = 3 * 'a';
-
-if (test == true) {
-	console.log('+++');
-} else {
-	console.log('');
+	console.log('---');
 }

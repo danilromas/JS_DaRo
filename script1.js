306e4dfd1999f5dd2418c8da00fe50e25e0a00ee
danilromas <furiblack904@gmail.com>
2023-03-01 10:04:17 +0300

    91_сложные_условия

--- script1.js
+++ script1.js
@@ -1,55 +1,73 @@
 "use strict";
 
-// let test = null;
+let test = true;
+   let test = true;
 
-// if (test) {
-// 	console.log('+++');
-// } else {
-// 	console.log('---');
-// }
-// ---
+   if (test && test) {
+      console.log('+++');
+   } else {
+      console.log('');
+   }
 
-// let test = false;
 
-// if (test) {
-// 	console.log('+++');
-// } else {
-// 	console.log('---');
-// }
-// ---
+   let test = true;
+   let test = true;
 
-// let test;
+   if (test && (!test)) {
+      console.log('+++');
+   } else {
+      console.log('');
+   }
 
-// if (test) {
-// 	console.log('+++');
-// } else {
-// 	console.log('---');
-// }
-// ---
 
-// let test = 0;
+   let test = true;
+   let test = true;
 
-// if (test) {
-// 	console.log('+++');
-// } else {
-// 	console.log('---');
-// }
-// ---
+   if ((!test) && (!test)) {
+      console.log('+++');
+   } else {
+      console.log(''); 
 
-// let test = '0';
+   }
 
-// if (test) {
-// 	console.log('+++');
-// } else {
-// 	console.log('---');
-// }
-// +++
+   
 
-// let test = -1;
+   let test = true;
+   let test = true;
 
-// if (test) {
-// 	console.log('+++');
-// } else {
-// 	console.log('---');
-// }
-// +++
+   if ((!test) && test) {
+      console.log('+++');
+   } else {
+      console.log('');
+   }
+
+
+   let test = true;
+   let test0 = true;
+   let test1 = true;
+
+   if (test && test0 && test1) {
+      console.log('+++');
+   } else {
+      console.log('');
+   }
+
+
+   let test2 = true;
+   let test = true;
+   let test = true;
+
+   if (test2 || test && test) {
+      console.log('+++');
+   } else {
+      console.log('');
+   }
+   let test = true;
+   let test = true;
+   let test = true;
+
+   if (test || (!test) && (!test)) {
+      console.log('+++');
+   } else {
+      console.log(''); 
+   }

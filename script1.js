1190cd7f922fc11f9565f626cbcd06c28e77c354
danilromas <furiblack904@gmail.com>
2023-03-01 09:55:31 +0300

    83_приоритетыыы

--- script1.js
+++ script1.js
@@ -1,7 +1,23 @@
 "use strict";
 let num = 3;
+		
+if (num > 5 && num < 10 || num == 20) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
 
-if (num > 0 && num < 5 || num > 10 && num < 20) {
+let num1 = 3;
+		
+if (num1 > 5 || num1 > 0 && num1 < 3) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+let num2 = 3;
+		
+if (num2 == 9 || num2 > 10 && num2 < 20 || num2 > 20 && num2 < 30) {
 	console.log('+++');
 } else {
 	console.log('---');

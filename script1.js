f7bbe55471ac158022cd2c4bbf8b2e593b537510
danilromas <furiblack904@gmail.com>
2023-03-01 09:11:41 +0300

    72_изучение_if

--- script1.js
+++ script1.js
@@ -1,17 +1,13 @@
 "use strict";
 
-let obj = {x: 1, y: 2, z: 3};
-console.log(obj);
-
-let obj1 = {x: 1, y: 2, z: 3};
-let key = 'z';
-
-console.log(obj1.key);
-
-let obj2 = {x: 1, y: 2, z: 3};
-let sum = obj[x] + obj[y] + obj[z];
-
-console.log(obj2);
-
-let obj3 = {x: 1, y: 2, z: 3};
-console.log(length.obj3);
+if (операция сравнения) {
+	/*
+		расположенный здесь код выполниться,
+		если результат операция сравнения верный
+	*/
+} else {
+	/*
+		расположенный здесь код выполниться,
+		если результат операция сравнения неверный
+	*/
+}

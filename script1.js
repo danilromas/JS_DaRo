705a696ebce5cb185931385ddf783db1bff63de3
danilromas <furiblack904@gmail.com>
2023-03-01 09:53:13 +0300

    81_&&

--- script1.js
+++ script1.js
@@ -1,8 +1,16 @@
 "use strict";
-let test1 = '3';
-let test2 = '3';
-		
-if (test1 != test2) {
+let num = 3;
+
+if (num > 0 && num < 10) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+let num1 = 2;
+let num2 = 3;
+
+if (num1 == 2 && num2 == 3) {
 	console.log('+++');
 } else {
 	console.log('---');

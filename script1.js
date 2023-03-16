9f1d82a2e39f007eadeff8d690a48fd863aa2c98
danilromas <furiblack904@gmail.com>
2023-03-16 16:56:05 +0300

    166

--- script1.js
+++ script1.js
@@ -1,32 +1,37 @@
 "use strict";
-function func(arr) {
-	let sum = 0;
+function func(num) {
+	let res;
 	
-	for (let i = 0; i < arr.length; i++) {
-		sum += arr[i];
-		
-		// Если сумма больше или равна 10:
-		if (sum >= 10) {
-			return i + 1; // выходим из цикла и из функции
-		}
+	if (num >= 0) {
+		res = Math.sqrt(num);
+	} else {
+		res = 0;
+	}
+	
+	return res;
+}
+
+console.log(func(3));
+function func(num) {
+	if (num >= 0) {
+		return Math.sqrt(num);
+	} else {
+		return 0;
 	}
 }
 
-let res = func([1, 2, 3, 4, 5]);
-console.log(res);
-function func() {
-	let sum = 0;
-	let i = 1;
+console.log(func(3));
+
+function func(num1, num2) {
+	let res;
 	
-	while (true) { // бесконечный цикл
-		sum += i;
-		
-		if (sum >= 100) {
-			return i; // цикл крутится пока не выйдет тут
-		}
-		
-		i++;
+	if (num1 > 0 && num2 > 0) {
+		res = num1 * num2;
+	} else {
+		res = num1 - num2;
 	}
+	
+	return res;
 }
 
-console.log( func() );
+console.log(func(3, 4));

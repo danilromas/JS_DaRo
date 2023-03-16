43bc9a8a4eb42e22f18197126bf5751a666a683f
danilromas <furiblack904@gmail.com>
2023-03-16 16:43:45 +0300

    151

--- script1.js
+++ script1.js
@@ -1,31 +1,51 @@
 "use strict";
-let affairs = {
-	'2019-12-28': ['data11', 'data12', 'data13'],
-	'2019-12-29': ['data21', 'data22', 'data23'],
-	'2019-12-30': ['data31', 'data32', 'data33'],
- };
- 
- 
- affairs['2019-12-29'].push('data24');
- 
- console.log(affairs)
- 
- 
- affairs['2019-12-31'] = ['data41', 'data42'];
- 
- console.log(affairs)
- 
- let students = {
-	'group1': {
-	   'subgroup11': ['student111', 'student112', 'student113'],
-	   'subgroup12': ['student121', 'student122', 'student123'],
-	},
-	'group2': {
-	   'subgroup21': ['student211', 'student212', 'student213'],
-	   'subgroup22': ['student221', 'student222', 'student223'],
-	},
-	'group3': {
-	   'subgroup31': ['student311', 'student312', 'student313'],
-	   'subgroup32': ['student321', 'student322', 'student323'],
-	},
- };
+console.log(Math.pow(2, 10));
+console.log(Math.sqrt(245));
+let arr = [4, 2, 5, 19, 13, 0, 10];
+let result = 0;
+for (let item of arr) {
+   result += (Math.pow(item, 3));
+}
+console.log(Math.sqrt(result));
+let num = 245;
+let result = Math.round(Math.sqrt(num));
+console.log(result
+  );
+let result = Math.sqrt(num);
+console.log(result.toPrecision(3));
+
+let result = Math.sqrt(num);
+console.log(result.toPrecision(4));
+
+let num1 = 587;
+let obj = {
+   floor: null,
+   ceil: null,
+}
+let result = Math.sqrt(num)
+obj.floor = Math.floor(result);
+let result = Math.sqrt(num);
+obj.ceil = Math.ceil(result);
+
+console.log(obj)
+
+console.log(Math.max(4, -2, 5, 19, -130, 0, 10));
+console.log(Math.min(4, -2, 5, 19, -130, 0, 10));
+
+
+function getRandomInt(min, max) {
+   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+console.log(getRandomInt(1, 100))
+
+let newArr = [];
+
+for (let i = 1; i <= 10; i++) {
+   newArr.push(getRandomInt(1, 1000));
+};
+
+console.log(newArr)
+let a = -12, b = -6;
+
+console.log(Math.abs(a - b));

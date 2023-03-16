2567a2066d423a9ab496841e82b4e2bc9773d53e
danilromas <furiblack904@gmail.com>
2023-03-16 16:43:08 +0300

    150

--- script1.js
+++ script1.js
@@ -1,26 +1,31 @@
 "use strict";
-let employees = [
-	{
-	   name: 'name1',
-	   salary: 300,
-	   age: 28,
+let affairs = {
+	'2019-12-28': ['data11', 'data12', 'data13'],
+	'2019-12-29': ['data21', 'data22', 'data23'],
+	'2019-12-30': ['data31', 'data32', 'data33'],
+ };
+ 
+ 
+ affairs['2019-12-29'].push('data24');
+ 
+ console.log(affairs)
+ 
+ 
+ affairs['2019-12-31'] = ['data41', 'data42'];
+ 
+ console.log(affairs)
+ 
+ let students = {
+	'group1': {
+	   'subgroup11': ['student111', 'student112', 'student113'],
+	   'subgroup12': ['student121', 'student122', 'student123'],
 	},
-	{
-	   name: 'name2',
-	   salary: 400,
-	   age: 29,
+	'group2': {
+	   'subgroup21': ['student211', 'student212', 'student213'],
+	   'subgroup22': ['student221', 'student222', 'student223'],
 	},
-	{
-	   name: 'name3',
-	   salary: 500,
-	   age: 30,
+	'group3': {
+	   'subgroup31': ['student311', 'student312', 'student313'],
+	   'subgroup32': ['student321', 'student322', 'student323'],
 	},
-  ];
-  employees.push(
-	{
-	   name: 'name4',
-	   salary: 600,
-	   age: 31,
-	}
-  );
-  console.log(employees);
+ };

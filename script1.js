fb91224f2a4d19da39140281e8f52d06d21346cb
danilromas <furiblack904@gmail.com>
2023-03-16 16:35:11 +0300

    142

--- script1.js
+++ script1.js
@@ -1,18 +1,33 @@
 "use strict";
-   for (let j = 1; j <= 5; j++) {
-      arr[i].push(j);
+for (let i = 0, k = 2; i < 3; i++) {
+	   for (let j = 0; j < 3; j++) {
+      arr[i].push(k++);
    }
+}
 
 console.log(arr);
 
 let arr = [];
-
-for (let i = 0; i < 3; i++) {
+let k = 0;
+for (let i = 0; i < 4; i++) {
    arr[i] = [];
-
-   for (let j = 1; j <= 5; j++) {
-      arr[i].push(j);
+   for (let j = 0; j < 3; j++) {
+      k += 2
+      arr[i].push(k);
    }
 }
+
+console.log(arr);
+
+let arr = [];
+for (let i = 0, p = 1; i < 2; i++) {
+   arr[i] = []
+   for (let j = 0; j < 2; j++) {
+      arr[i][j] = [];
+      for (let k = 0; k < 2; k++) {
+         arr[i][j].push(p++);
+      };
+   };
+};
 
 console.log(arr);

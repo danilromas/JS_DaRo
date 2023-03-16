20b77678b9f3abd105f830748cac932ee99a88b3
danilromas <furiblack904@gmail.com>
2023-03-16 16:39:52 +0300

    147

--- script1.js
+++ script1.js
@@ -1,53 +1,39 @@
 "use strict";
-let data = {
-	1: [
-	   'data11',
-	   'data12',
-	   'data13',
-	],
-	2: [
-	   'data21',
-	   'data22',
-	   'data23',
-	],
-	3: [
-	   'data31',
-	   'data32',
-	   'data33',
-	],
-	4: [
-	   'data41',
-	   'data42',
-	   'data43',
-	],
- };
- for (let key in data) {
-	let fullData = data[key]
-	for (let item of fullData) {
-	   console.log(item)
-	}
- };
- 
- let data1 = [
-	{
-	   1: 'data11',
-	   2: 'data12',
-	   3: 'data13',
-	},
-	{
-	   1: 'data21',
-	   2: 'data22',
-	   3: 'data33',
-	},
-	{
-	   1: 'data31',
-	   2: 'data32',
-	   3: 'data33',
-	},
- ];
- 
- for (let key of data1) {
-	for (let item in key) {
-	   console.log(key[item])
-	}
- };
+let employees = [
+   {
+      name: 'name1',
+      salary: 300,
+   },
+   {
+      name: 'name2',
+      salary: 400,
+   },
+   {
+      name: 'name3',
+      salary: 500,
+   },
+];
+for (let item of employees) {
+   console.log(item.name + ' ' + '-' + ' ' + item.salary)
+}
+let employees2 = [
+   {
+      name: 'name1',
+      salary: 300,
+   },
+   {
+      name: 'name2',
+      salary: 400,
+   },
+   {
+      name: 'name3',
+      salary: 500,
+   },
+];
+let sum = 0;
+
+for (let item of employees) {
+   sum += item.salary;
+};
+
+console.log(sum)

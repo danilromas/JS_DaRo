558a545002d9d44a0629cea3314f310396a2a050
danilromas <furiblack904@gmail.com>
2023-03-16 16:31:38 +0300

    140

--- script1.js
+++ script1.js
@@ -1,23 +1,17 @@
 "use strict";
-let letarr = [
-	[
-	  [1, 2],
-	  [3, 4],
-	],
-	[
-	  [5, 6],
-	  [7, 8],
-	],
-  ];
- 
-  let sum = 0;
- 
-  for (let i = 0; i < letarr.length; i++) {
-	for (let j = 0; j < letarr[i].length; j++) {
-	  for (let k = 0; k < letarr[i][j].length; k++) {
-		sum += letarr[i][j][k];
-	  }
-	}
-  }
- 
-  console.log(sum);
+let newArr = [];
+for (let i = 0; i <= 2; i++) {
+   newArr[i] = [];
+   for (let k = 1; k <= 5; k++) {
+      newArr[i].push(k)
+   };
+};
+console.log(newArr);
+let newArr = [];
+for (let i = 0; i <= 2; i++) {
+   newArr[i] = [];
+   for (let k = 1; k <= 4; k++) {
+      newArr[i].push('x')
+   };
+};
+console.log(newArr);

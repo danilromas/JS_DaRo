fb198dca943d406fdd96cd3df2262b6c37a941fb
danilromas <furiblack904@gmail.com>
2023-03-16 16:50:08 +0300

    161

--- script1.js
+++ script1.js
@@ -1,13 +1,7 @@
 "use strict";
-function func(num = 5) {
-	console.log(num * num);
- }
-func();
-func(6);
-func(1); 
-function function1(num1 = 0, num2 = 0) {
-	console.log(num1 + num2);
- };
-function1(2, 3); 
-function1(3);    
-function1();  
+function calcCube(x) {
+	return Math.pow(x, 3)
+  };
+  let res = calcCube(3);
+
+console.log(res)

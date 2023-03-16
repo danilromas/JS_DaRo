f3cf6cddc97554e394cc536a812f98703e580aef
danilromas <furiblack904@gmail.com>
2023-03-16 15:47:51 +0300

    106

--- script1.js
+++ script1.js
@@ -1,25 +1,8 @@
 "use strict";
 
-let string = 'dsdqwdqwd';
-
-if (string[0] == 'D' || string[0] == 'd') {
-   console.log('da');
-} else {
-   console.log('net');
-}
-
-let string1 = '2dfasdfa43afe3';
-
-if (string1[string1.length - 1] == '3' || string1[string1.length - 1] == '3') {
-   console.log('da');
-} else {
-   console.log('net');
-}
-
-let string2 = 'tfsdfaw4fa4';
-
-if (string2[0] == 't' || string2[0] == 'f') {
-   console.log('da');
-} else {
-   console.log('net');
-}
+let num = 4;
+if (num % 2 == 0) {
+	console.log('da')
+ } else {
+	console.log('net')
+ }

7ea7a0b0b4b3f306a16a9f8d80b514b5458a46c8
danilromas <furiblack904@gmail.com>
2023-03-16 15:58:11 +0300

    119

--- script1.js
+++ script1.js
@@ -1,18 +1,7 @@
 "use strict";
 
-let obj = { a: 1, b: 2, c: 3, d: 4, e: 5 };
-let res2 = 1;
+let str = '-';
 
-for (let i = 1; i <= 20; i++) {
-   res = res * i
-}
-
-console.log(res)
-
-
-let arr = [2, 5, 9, 3, 1, 4];
-
-for (let i = 0; i <= arr.length - 1; i++) {
-   res4 = res4 * arr[i] 
+for (let i = 1; i <= 9; i++) {
+   str +=  String(i) + '-' 
 };
-console.log(res4)

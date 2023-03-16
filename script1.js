4c0f15190d2f6741e591cc55dc1ee0f2fe37c0a7
danilromas <furiblack904@gmail.com>
2023-03-16 16:39:08 +0300

    146

--- script1.js
+++ script1.js
@@ -1,7 +1,53 @@
 "use strict";
-let students = {
-	'group1': ['Данюшис', 'Ромашкан', 'Авагян'],
-	'group2': ['Элвин', 'Виталий', 'Эдем'],
-	'group3': ['Арсен', 'Павел', 'Андрей'],
+let data = {
+	1: [
+	   'data11',
+	   'data12',
+	   'data13',
+	],
+	2: [
+	   'data21',
+	   'data22',
+	   'data23',
+	],
+	3: [
+	   'data31',
+	   'data32',
+	   'data33',
+	],
+	4: [
+	   'data41',
+	   'data42',
+	   'data43',
+	],
  };
- console.log(students['group3'][0])
+ for (let key in data) {
+	let fullData = data[key]
+	for (let item of fullData) {
+	   console.log(item)
+	}
+ };
+ 
+ let data1 = [
+	{
+	   1: 'data11',
+	   2: 'data12',
+	   3: 'data13',
+	},
+	{
+	   1: 'data21',
+	   2: 'data22',
+	   3: 'data33',
+	},
+	{
+	   1: 'data31',
+	   2: 'data32',
+	   3: 'data33',
+	},
+ ];
+ 
+ for (let key of data1) {
+	for (let item in key) {
+	   console.log(key[item])
+	}
+ };

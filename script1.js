ece317ccf25b7418dff2fc2d38b353eb38ed0a49
danilromas <furiblack904@gmail.com>
2023-03-16 16:16:02 +0300

    137

--- script1.js
+++ script1.js
@@ -1,13 +1,2 @@
 "use strict";
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-let newArr = []
-
-for (let elem of arr13) {
-   if (elem % 2 != 0) {
-      newArr.push(elem);
-   } else {
-      continue;
-	}
-}
-
-console.log(newArr);
+//переход

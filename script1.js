66e25f3c199b593d36fc4361880b4c2b6f9ee176
danilromas <furiblack904@gmail.com>
2023-03-16 15:58:35 +0300

    120

--- script1.js
+++ script1.js
@@ -1,7 +1,22 @@
 "use strict";
-
-let str = '-';
-
-for (let i = 1; i <= 9; i++) {
-   str +=  String(i) + '-' 
-};
+for (let i = 10; i <= 1000; i++) {
+	res = String(i);
+	console.log(res1[0]);
+ };
+ 
+ 
+ let res = 0;
+ for (let i = 10; i <= 1000; i++) {
+	str = String(i);
+	res = Number(str[0]) + Number(str[1])
+	console.log(res);
+ };
+ 
+ for (let i = 10; i <= 1000; i++) {
+	str = String(i);
+	if (str[0] == 1) {
+	   console.log(Number(str));
+	} else {
+	   continue;
+	};
+ };

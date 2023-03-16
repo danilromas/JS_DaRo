61a6d995535de0ce84438611f7f7355fa5dabccf
danilromas <furiblack904@gmail.com>
2023-03-16 15:50:38 +0300

    109

--- script1.js
+++ script1.js
@@ -1,11 +1,10 @@
 "use strict";
+let num15 = '123033';
+let sum1 = Number(num15[0]) + Number(num15[1]) + Number(num15[2]);
+let sum2 = Number(num15[3]) + Number(num15[4]) + Number(num15[5]);
 
-let num = '432342';
-
-   let sum1 = Number(num[0]) + Number(num[1]) + Number(num[2]);
-   let sum2 = Number(num[3]) + Number(num[4]) + Number(num[5]);
    if (sum1 == sum2) {
-	console.log('summaries equal');
- } else 
- {
-	console.log('summaries not equal');}
+      console.log('суммы равны');
+   } else {
+	console.log('суммы не равны');
+   }

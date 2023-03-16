2062010065908136c2961ec2e7c962b55043bc6f
danilromas <furiblack904@gmail.com>
2023-03-16 16:56:31 +0300

    167

--- script1.js
+++ script1.js
@@ -1,37 +1,21 @@
 "use strict";
-function func(num) {
-	let res;
+function isPositive(arr) {
+	let flag = true;
 	
-	if (num >= 0) {
-		res = Math.sqrt(num);
-	} else {
-		res = 0;
+	for (let elem of arr) {
+		if (elem < 0) {
+			flag = false;
+		}
 	}
 	
-	return res;
+	return flag;
 }
-
-console.log(func(3));
-function func(num) {
-	if (num >= 0) {
-		return Math.sqrt(num);
-	} else {
-		return 0;
-	}
-}
-
-console.log(func(3));
-
-function func(num1, num2) {
-	let res;
-	
-	if (num1 > 0 && num2 > 0) {
-		res = num1 * num2;
-	} else {
-		res = num1 - num2;
+function isPositive(arr) {
+	for (let elem of arr) {
+		if (elem < 0) {
+			return false;
+		}
 	}
 	
-	return res;
+	return true;
 }
-
-console.log(func(3, 4));

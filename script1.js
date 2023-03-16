4d80a602ca3d540d5560e5ed3ae991da92a330be
danilromas <furiblack904@gmail.com>
2023-03-16 16:49:44 +0300

    160

--- script1.js
+++ script1.js
@@ -1,8 +1,13 @@
 "use strict";
-let aa = 1;
-let bb = 5;
-let cc = 3;
-function three(a, b, c) {
-	return console.log(a + b + c);
-  }
-  three(aa, bb, cc);
+function func(num = 5) {
+	console.log(num * num);
+ }
+func();
+func(6);
+func(1); 
+function function1(num1 = 0, num2 = 0) {
+	console.log(num1 + num2);
+ };
+function1(2, 3); 
+function1(3);    
+function1();  

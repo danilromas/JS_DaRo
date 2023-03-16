939fce33dd6516a78b2fc17a743e148ab3d51727
danilromas <furiblack904@gmail.com>
2023-03-16 16:01:39 +0300

    123

--- script1.js
+++ script1.js
@@ -1,17 +1,7 @@
 "use strict";
-let num = 1, i = 1;
-
-for (i; ; i++) {
-   if (num + i > 100) {
-      break;
-   if (elem % 2 == 0) {
-	result = elem * elem;
- } else if (elem % 3 == 0) {
-	result = elem * elem * elem;
-} else
-	{continue;
-}
-
-console.log(result);
-	}
-}
+for (let i = 1; i <= 3; i++) {
+	document.write(' ')
+	for (let k = 1; k <= 3; k++) {
+	   document.write(i + k + ' ');
+	};
+ };

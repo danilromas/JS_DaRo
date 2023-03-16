96d23a3f7bfd6b34bb2931e28864f065dfbd082d
danilromas <furiblack904@gmail.com>
2023-03-16 16:16:43 +0300

    138

--- script1.js
+++ script1.js
@@ -1,2 +1,20 @@
 "use strict";
-//переход
+let arr = [
+		['a', 'b', 'c'],
+		['d', 'e', 'f'],
+		['g', 'h', 'i'],
+		['j', 'k', 'l'],
+	];
+	
+	console.log(arr[3][2] + arr[1][1] + arr[2][0] +arr[0][0]);
+	
+	let later = [[1, 2], [3, 4], [5, 6]];
+	let sum = 0;
+	
+	for (let i = 0; i < later.length; i++) {
+	  for (let j = 0; j < later[i].length; j++) {
+	    sum += later[i][j];
+	  }
+	}
+	
+	console.log(sum);

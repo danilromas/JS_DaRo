8e623787ebe17f1229f149871989063eca72c3ec
danilromas <furiblack904@gmail.com>
2023-03-16 16:04:11 +0300

    125

--- script1.js
+++ script1.js
@@ -1,14 +1,7 @@
 "use strict";
-for (let i = 1; i <= 9; i++) {
-	let num = 3;
+for (let i = 0; i <= 9; i++) {
+	for (let j = 0; j <= 9; j++) {
+	   let num = 3;
+	}
+}
 	console.log(num);
- }
- for (let i = 1; i <= 9; i++) {
-	let num = 3;
- }
- console.log(num);
-  let num;
- for (let i = 1; i <= 9; i++) {
-	num = 3;
- }
- console.log(num);

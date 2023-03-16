aa057c37953d37ad4c5877e727325038e91150aa
danilromas <furiblack904@gmail.com>
2023-03-16 15:56:43 +0300

    117

--- script1.js
+++ script1.js
@@ -1,11 +1,10 @@
 "use strict";
 
 let obj = { a: 1, b: 2, c: 3, d: 4, e: 5 };
+let res2 = 1;
 
-for (let key in obj) {
-	if (obj[key] % 2 == 0) {
-	   console.log(obj[key]);
-	} else {
-	   continue;
-	}
- }
+for (let i = 1; i <= 20; i++) {
+   res = res * i
+}
+
+console.log(res)

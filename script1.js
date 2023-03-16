9867c5518213f662a550140e3c642ae02033ac36
danilromas <furiblack904@gmail.com>
2023-03-16 16:53:01 +0300

    165

--- script1.js
+++ script1.js
@@ -1,24 +1,32 @@
 "use strict";
-function func() {
+function func(arr) {
 	let sum = 0;
 	
-	for (let i = 1; i <= 5; i++) {
-		sum += i;
-	}
-	
-	return sum;
-}
-
-let res = func();
-console.log(res);
-function func() {
-	let sum = 0;
-	
-	for (let i = 1; i <= 5; i++) {
-		sum += i;
-		return sum;
+	for (let i = 0; i < arr.length; i++) {
+		sum += arr[i];
+		
+		// Если сумма больше или равна 10:
+		if (sum >= 10) {
+			return i + 1; // выходим из цикла и из функции
+		}
 	}
 }
 
-let res = func();
+let res = func([1, 2, 3, 4, 5]);
 console.log(res);
+function func() {
+	let sum = 0;
+	let i = 1;
+	
+	while (true) { // бесконечный цикл
+		sum += i;
+		
+		if (sum >= 100) {
+			return i; // цикл крутится пока не выйдет тут
+		}
+		
+		i++;
+	}
+}
+
+console.log( func() );

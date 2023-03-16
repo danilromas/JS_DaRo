b47cf8051ad46b7f7b688f5c35d8784377905fb9
danilromas <furiblack904@gmail.com>
2023-03-16 15:49:33 +0300

    108

--- script1.js
+++ script1.js
@@ -1,8 +1,11 @@
 "use strict";
 
-let num = 30;
-if (num % 3 == 0) {
-	console.log('da')
- } else {
-	console.log('net')
- }
+let num = '432342';
+
+   let sum1 = Number(num[0]) + Number(num[1]) + Number(num[2]);
+   let sum2 = Number(num[3]) + Number(num[4]) + Number(num[5]);
+   if (sum1 == sum2) {
+	console.log('summaries equal');
+ } else 
+ {
+	console.log('summaries not equal');}

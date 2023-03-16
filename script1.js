c0d1a1ed88a74a72cc88a2ee983c8354197513f0
danilromas <furiblack904@gmail.com>
2023-03-16 16:38:30 +0300

    145

--- script1.js
+++ script1.js
@@ -1,26 +1,7 @@
 "use strict";
-let obj = {
-	1: {
-	   1: 11,
-	   2: 12,
-	   3: 13,
-	},
-	2: {
-	   1: 21,
-	   2: 22,
-	   3: 23,
-	},
-	3: {
-	   1: 24,
-	   2: 25,
-	   3: 26,
-	},
+let students = {
+	'group1': ['Данюшис', 'Ромашкан', 'Авагян'],
+	'group2': ['Элвин', 'Виталий', 'Эдем'],
+	'group3': ['Арсен', 'Павел', 'Андрей'],
  };
- let result = 0;
- for (let key in obj) {
-	let obj3 = obj[key];
-	for (let obj1 in obj3) {
-	   result += obj3[obj1]
-	};
- };
- console.log(result)
+ console.log(students['group3'][0])

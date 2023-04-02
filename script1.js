f125638c586269c353ff2bab66fae0d4b0bd2770
danilromas <furiblack904@gmail.com>
2023-04-02 19:02:58 +0300

    169

--- script1.js
+++ script1.js
@@ -1,13 +1,30 @@
 "use strict";
-function func(a, b) {
-	return a == b
+function sum(arr) { // деление тут не нужно
+	let res = 0;
+	
+	for (let elem of arr) {
+		res += elem;
+	}
+	
+	return res;
 }
-function func(a, b) {
-	return a != b
+
+function func(arr1, arr2) { // можно сделать всё одним циклом for, да и смысла код не имеет, в любом случае выведет 1
+	let res1 = 0;
+	let res2 = 0;
+	
+	for (let elem of arr) {
+		res1 += elem;
+		res2 += elem;
+	}
+	return res1 / res2;
 }
-function func(a, b) {
-	return a + b >=10
-}
-function func(num) {
-	return a >= 0 
+
+function getSum(arr) { // код выведет в любом случае 0, поэтому нужно вписать в res 1
+	let res = 1;
+	for (let elem of arr) {
+		res *= elem;
+	}
+	
+	return res;
 }
